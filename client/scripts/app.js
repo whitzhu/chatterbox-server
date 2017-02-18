eb97a46c7dd62c61af29ce1caf049f189cf3c813
whitzhu <whitzhu@gmail.com>
2017-02-17 16:42:30 -0800

    Pass Mocha tests, add an add room form

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,16 @@
+// $(document).ready(app.init);
+
 var app = {
   init: function() {
+    //$(user) --> handleUsernameClick()
+    $('.username').on('click', app.handleUsernameClick);
+    $('#send .submit').on('submit', app.handleSubmit);
 
+    $('#addroom').click(function(event) {
+      event.preventDefault();
+      var newRoomName = $('#newroom').val();
+      app.renderRoom(newRoomName);
+    });
   }, 
 
   send: function(message) {
@@ -21,11 +31,12 @@ var app = {
   fetch: function(source) {
     $.ajax({
       url: source,
-      type: 'POST',
+      type: 'GET',
       //data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Data fetched');
+        console.log(data);
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch data', data);
@@ -38,10 +49,23 @@ var app = {
   },
 
   renderMessage: function(message) {
-    $('#chats').append( `<p> ${message.text} </p>` );
+    $('#chats').append( `<p class="chat"> <span class="username"> ${message.user} </span> ${message.text} </p>` );
   },
 
   renderRoom: function(name) {
+    console.log(name);
     $('#roomSelect').append( `<option value= ${name}>${name}</option>`);
+  },
+
+  handleUsernameClick: function() {
+
+  },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
   }
+
 };
+
+$(document).ready(app.init);
+
