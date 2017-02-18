99aa8cefbaa898d327bca619322d4a0422372320
whitzhu <whitzhu@gmail.com>
2017-02-17 21:14:27 -0800

    Implement room filter and add to room chat
    Bugs: Room filter needs to list all unique options available
    Idea: Default to all (no filter)
    To implement: when making a new room, automatically move to that room
    XSS protection!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -19,15 +19,17 @@ var app = {
       var message = {
         username: username,
         text: $('#message').val(),
-        roomname: 'lobby'
+        roomname: $('#roomSelect option:selected').val()
       };
       app.send(message);
       $('#message').val('');
       app.fetch();
     });
 
-    setInterval(app.fetch, 5000);
-    // app.fetch();
+    $('#roomSelect').on('change', app.fetch);
+
+    //setInterval(app.fetch, 5000);
+    app.fetch();
   }, 
 
   send: function(message) {
@@ -45,20 +47,41 @@ var app = {
     });
   },
 
+  //uniqueRooms: {}, 
+
   fetch: function() {
     $.ajax({
       url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
       type: 'GET',
-      data: {order: '-createdAt', limit: '20' },
+      data: {
+        order: '-createdAt', //ADD NEGATIVE LATER
+        limit: '100',
+        where: {
+          'roomname': $('#roomSelect option:selected').val()
+        }
+      },
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Data fetched');
         console.log(data);
         $('#chats').html('');
         var messagesArray = data.results;
+        var uniqueRooms = {};
         messagesArray.forEach( function(element) {
           app.renderMessage(element);
+          if (!uniqueRooms.hasOwnProperty(element.roomname)) {
+            uniqueRooms[element.roomname] = 1;
+          }
         });
+        //console.log('THIS IS UNIQUE ROOMS: ', uniqueRooms);
+        //console.log('ROOM SELECT: ', typeof $('#roomSelect').val());
+        /*$('#roomSelect option').each(function() {
+          for (var key in uniqueRooms) {
+            if ($(this).val() !== key) {
+              app.renderRoom(key);
+            }
+          }
+        });*/
       },
       error: function (data) {
         console.error('chatterbox: Failed to fetch data', data);
@@ -71,11 +94,11 @@ var app = {
   },
 
   renderMessage: function(message) {
-    $('#chats').append( `<p class="chat"> <span class="username"> ${message.username} </span> ${message.text} ${message.roomname} </p>` );
+    $('#chats').append( `<p class="chat"> <span class="username"> ${JSON.stringify(message.username)} </span> ${JSON.stringify(message.text)} ${JSON.stringify(message.roomname)} </p>` );
   },
 
   renderRoom: function(name) {
-    $('#roomSelect').append( `<option value= ${name}>${name}</option>`);
+    $('#roomSelect').append( `<option value=${name}>${name}</option>`);
   },
 
   handleUsernameClick: function() {
