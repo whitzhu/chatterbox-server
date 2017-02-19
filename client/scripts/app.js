f325faa054d698ed3b91d6acbbeae5ecdeacab20
whitzhu <whitzhu@gmail.com>
2017-02-19 00:59:13 -0800

    updating css styling and organized app.js code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,63 +1,39 @@
 var app = {
-  init: function() {
+  src: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/',
+  username: window.location.search.slice(10),
+  uniqueRooms: {}, 
+  id: '',
+  firstLoad: true,
+  friend: [],
 
-    var src = 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages/';
-    var userName = window.location.search.slice(10);
-    var room = {
-      'roomname': $('#roomSelect option:selected').text()
+  init: function() {
+    app.triggerAddRoom();
+    app.cancelAddRoom();
+    app.addNewRoom();
+    app.messagePanelHeader();
+    app.filterRoom();
+    app.sendMessage();
+    app.profilePanel();
+    
+    $('#chats').on('click', '.username', app.handleUsernameClick);
+    
+    var roomFilter = {
+      order: '-createdAt', 
+      limit: '100',
+      where: {
+        'roomname': $('#roomSelect option:selected').text()
+      }
     };
 
-    $('#chats').on('click', '.username', app.handleUsernameClick);
-
-    $('#send .submit').on('submit', app.handleSubmit);
-
-    $('#addroom').click(function(event) {
-      event.preventDefault();
-      var newRoomName = $('#newroom').val();
-      app.renderRoom(JSON.stringify(newRoomName));
-      app.fetch(src, {
-        'roomname': newRoomName
-      });
-      $(`#roomSelect option[value=${newRoomName}]`).attr('selected', 'selected');
-
-      $('#newroom').val('');
-    });
-
-    $('#send').on('click', function(event) {
-      event.preventDefault();
-      var message = {
-        username: userName,
-        text: $('#message').val(),
-        roomname: $('#roomSelect option:selected').text()
-      };
-      app.send(message);
-      $('#message').val('');
-      room = {
-        'roomname': $('#roomSelect option:selected').text()
-      };
-      setTimeout(function() {
-        app.fetch(src, room);
-      }, 1000);
-    });
-
-    $('#roomSelect').on('change', function() {
-      var rmn = $('#roomSelect option:selected').text();
-      if (rmn === 'All') {
-        rmn = {};
-      }
-      room = {
-        'roomname': rmn
-      };
-      app.clearMessages();
-      console.log('room', room);
-      app.firstLoad = true;
-      app.fetch(src, room);
-    });
+    var noRoomFilter = {
+      order: '-createdAt', 
+      limit: '100',
+    };   
     
-
-    app.fetch(src, {});
+    app.fetch(app.src, noRoomFilter);
+    
     setInterval(function() {
-      app.fetch(src, room);
+      app.fetch(app.src, roomFilter);
     }, 1000);
   }, 
 
@@ -76,63 +52,24 @@ var app = {
     });
   },
 
-  uniqueRooms: {}, 
-  id: '',
-  firstLoad: true,
   fetch: function(source, sortBy) {
     $.ajax({
       url: source,
       type: 'GET',
-      data: {
-        order: '-createdAt', 
-        limit: '100',
-        where: sortBy
-      },
+      data: sortBy,
       contentType: 'application/json',
       success: function (data) {
         console.log('chatterbox: Data fetched');
         //console.log(data);
-        var messagesArray = data.results;
-
-        // Populate a uniqueRooms object to keep track of individual rooms
-        messagesArray.forEach( function(element) {
-          var roomName = app.escapeHtml(element.roomname);
-          if (!app.uniqueRooms.hasOwnProperty(roomName)) {
-            app.uniqueRooms[roomName] = 1;
-          }
-        });
-
-        // Populate room drop down list
-        for (var room in app.uniqueRooms) {
-          app.renderRoom(room);
-        }
+        var dataArray = data.results;
 
         //Populate first load messages
         if (app.firstLoad) {
-          for (var i = messagesArray.length - 1; i >= 0; i--) {
-            app.renderMessage(messagesArray[i]);
-          }
-          app.id = messagesArray[0].objectId;
-          app.firstLoad = false;
+          app.initialLoadMessage(dataArray);
 
         } else {
           //Populate new messages
-          var flagNewMessage = false;
-          var sortMessage = _.sortBy(messagesArray, 'createdAt');
-          var filterMessage = _.filter(sortMessage, function(element){
-            if( flagNewMessage){
-              return element;
-            }
-            if(element.objectId === app.id) { 
-              flagNewMessage = true;
-            }
-          })
-          
-          filterMessage.forEach( function(element) {
-            app.renderMessage(element);
-          });
-          
-          app.id = messagesArray[0].objectId;
+          app.loadNewMessage(dataArray);
         }
       },
       error: function (data) {
@@ -140,8 +77,6 @@ var app = {
       }
     });
   },
-
-
 
   clearMessages: function() {
     $('#chats').html('');
@@ -151,24 +86,53 @@ var app = {
     var username = app.escapeHtml(message.username);
     var mess = app.escapeHtml(message.text);
     var room = app.escapeHtml(message.roomname);
+    var createTime = app.escapeHtml(message.createdAt);
     var friendClass = '';
     if ( app.friend.indexOf(username) > - 1) {
       friendClass = 'friend';
     } 
-    $('#chats').prepend( `<p class="chat"> <span class="username ${friendClass}">${username}</span> ${mess} ${room} </p>` );
+    $('#chats').prepend( `<p class="chat card-block"> <span class="username ${friendClass}">${username}</span><span class="room">${room}</span><span class="create-time">${createTime}</span><span class="message">${mess}</span></p>` );
   },
 
-  friend: [],
 
   renderRoom: function(name) {
-
-    var currentRoomList = []; 
+    var currentRoomList = [];
     $('#roomSelect option').each(function() {
       currentRoomList.push($(this).text());
     });
     if (currentRoomList.indexOf(name) === -1) {
       $('#roomSelect').append( `<option value=${name}>${name}</option>`);
     }
+  },
+
+  sendMessage: function() {
+    $('#send .submit').on('submit', app.handleSubmit);
+    
+    $('#send').on('click', function(event) {
+      event.preventDefault();
+      
+      var message = {
+        username: app.username,
+        text: $('#message').val(),
+        roomname: $('#roomSelect option:selected').text()
+      };
+
+      app.send(message);
+      
+      $('#message').val('');
+      
+      var roomFilter = {
+        order: '-createdAt', 
+        limit: '100',
+        where: {
+          'roomname': $('#roomSelect option:selected').text()
+        }
+      };
+      
+      setTimeout(function() {
+        app.fetch(app.src, roomFilter);
+      }, 1000);
+    });
   },
 
   handleUsernameClick: function() {
@@ -191,6 +155,133 @@ var app = {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;  
+  },
+
+  messagePanelHeader: function() {
+    var currentRoomName = $('#roomSelect option:selected').text();
+    var panelHeading = `<p>Post a message in <span class="current-room">${currentRoomName}</span> room</p>`; 
+    $('#main .panel-heading').html(panelHeading);
+  },
+
+  profilePanel: function() {
+    $('#side .username-title').text(app.username);
+  },
+
+  filterRoom: function() {
+    $('#roomSelect').on('change', function() {
+      var rmn = $('#roomSelect option:selected').text();
+      if (rmn === 'All') {
+        rmn = {};
+      }
+      var roomFilter = {
+        order: '-createdAt', 
+        limit: '100',
+        where: {
+          'roomname': rmn
+        }
+      };
+
+      app.clearMessages();
+      app.firstLoad = true;
+      app.messagePanelHeader();
+      app.fetch(app.src, roomFilter);
+    });
+  },
+
+  loadFilterRoom: function(array) {
+    array.forEach( function(element) {
+      var roomName = app.escapeHtml(element.roomname);
+      if (!app.uniqueRooms.hasOwnProperty(roomName)) {
+        app.uniqueRooms[roomName] = 1;
+      }
+    });
+
+    for (var room in app.uniqueRooms) {
+      app.renderRoom(room);
+    }
+  },
+
+  triggerAddRoom: function() {
+    $('#trigger-addroom').on('click', function(){
+      $('.roomSelect').hide();
+      $('#cancel-add-room').show();
+      $('#newroom').show();
+      $('#addroom').show();
+      $('#trigger-addroom').hide();
+    });
+  },
+
+  cancelAddRoom: function() {
+    $('#cancel-add-room').on('click', function(){
+      $('#cancel-add-room').hide();
+      $('#newroom').val('');
+      $('#newroom').hide();
+      $('#addroom').hide();
+      $('.roomSelect').show();
+      $('#trigger-addroom').show();
+    });
+  },
+
+  addNewRoom: function() {
+    $('#addroom').click(function(event) {
+      event.preventDefault();
+
+      var newRoomName = $('#newroom').val();
+      var roomFilter = {
+        order: '-createdAt', 
+        limit: '100',
+        where: {
+          'roomname': newRoomName
+        }
+      };
+      app.renderRoom(app.escapeHtml(newRoomName));
+
+      app.clearMessages();
+      app.firstLoad = true;
+      app.fetch(app.src, roomFilter);
+      
+      $(`#roomSelect option[value=${newRoomName}]`).attr('selected', 'selected');
+      $('#newroom').val('');
+      $('.roomSelect').show();
+      $('#cancel-add-room').hide();
+      $('#newroom').hide();
+      $('#addroom').hide();
+      $('#trigger-addroom').show();
+      app.messagePanelHeader();
+    });
+  },
+
+  initialLoadMessage: function(array) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      app.renderMessage(array[i]);
+    }
+    app.loadFilterRoom(array);
+    if (array[0] !== undefined ) {
+      app.id = array[0].objectId;
+    }
+    app.firstLoad = false;
+  },
+
+  loadNewMessage: function(array) {
+    var flagNewMessage = false;
+    var sortMessage = _.sortBy(array, 'createdAt');
+
+    var filterMessage = _.filter(sortMessage, function(element){
+      if( flagNewMessage){
+        return element;
+      }
+      if(element.objectId === app.id) { 
+        flagNewMessage = true;
+      }
+    });
+    
+    filterMessage.forEach( function(element) {
+      app.renderMessage(element);
+    });
+
+    if (array[0] !== undefined ) {
+      app.id = array[0].objectId;
+    }
   }
 
 };
